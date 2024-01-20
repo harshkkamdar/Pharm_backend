babc0e85821f47ea65981417d5fd00e00084c819
Harsh Kamdar <harshkkamdar@gmail.com>
2024-01-20 10:47:25 +0530

    MongoDB Connecction String Change

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose  = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://harshkkamdar:kWPrZpRPt3Yl3nz5@pharmacy.oflsnjx.mongodb.net/',{useNewURLParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://Harsh:admin@clusternyx.3vqwyny.mongodb.net/Pharm_Database',{useNewURLParser: true, useUnifiedTopology: true})
 .then(() => console.log("Database Connected..."))
 .catch((error) => console.log(error));
 
